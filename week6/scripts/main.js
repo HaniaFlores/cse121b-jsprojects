d523287491ea146cbbad1c09b69a3612b6d40a34
HaniaFlores <hanniaflores03@gmail.com>
2023-02-11 11:41:41 -0600

    Final Project

---
+++ week6/scripts/main.js
@@ -0,0 +1,61 @@
+let results = {};
+
+function calculateTaxes(bill, rate) {
+    return bill * (rate / 100);
+}
+
+function getTaxes() {
+    let billElement = parseFloat(document.getElementById("amount").value);
+    let taxRate = parseFloat(document.getElementById("tax-rate").value);
+    let taxes = calculateTaxes(billElement, taxRate);
+    results.billAmount = billElement;
+    results.salesTax = taxes;
+    return taxes;
+}
+
+function calculateTip(bill, rate) {
+    return bill * rate;
+}
+
+function getTip() {
+    let billElement = parseFloat(document.getElementById("amount").value);
+    let tipRate = parseFloat(document.getElementById("services").value);
+    let tip = calculateTip(billElement, tipRate);
+    results.tipAmount = tip;
+    return tip;
+}
+
+function calculateTotal(bill, taxes, tip) {
+    return bill + taxes + tip;
+}
+
+function getTotal() {
+    let bill = results.billAmount;
+    let taxes = results.salesTax;
+    let tip = results.tipAmount;
+    let total = calculateTotal(bill, taxes, tip);
+    results.total = total;
+}
+
+// *** RESULTS ***
+function displayResults() {
+    getTaxes();
+    getTip();
+    getTotal(); 
+    
+    document.querySelector(".results").style.display = "block";
+    document.getElementById("subtotal").textContent = results.billAmount.toFixed(2);
+    document.getElementById("tax-amount").textContent = results.salesTax.toFixed(2);
+    document.getElementById("tip-amount").textContent = results.tipAmount.toFixed(2);
+    document.getElementById("total").textContent = results.total.toFixed(2);
+}
+
+
+document.getElementById("calculate").addEventListener("click", () => {
+    displayResults();
+});
+
+const d = new Date();
+const year = d.getFullYear()
+document.getElementById("year").innerHTML = year;
+
